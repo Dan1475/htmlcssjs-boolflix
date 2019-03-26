4deeecd043eb3063c21e404ad6b724006c0c73d2
Daniela Ortenzi <daniela.ortenzi92@gmail.com>
2019-03-26 13:52:30 +0100

    milestone 2

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 
-function addTitle(title, original, lang, voto){
-
+function addTitle(title, original, flag, voto){
 
   var tempData = {
     title: title,
     original: original,
-    lang: lang,
-    voto: voto
+    flag: flag
   }
+
   var template = $('#box-template').html();
   var compiled = Handlebars.compile(template);
   var finalHTML = compiled(tempData);
 
-  var ulFilms = $('.films');
+  var ulFilms = $('.result');
   ulFilms.append(finalHTML);
+  addStar(voto);
 }
-
 
 function ajaxCall(title){
 
@@ -43,6 +42,66 @@ function ajaxCall(title){
   })
 }
 
+function ajaxSeries(title){
+
+  var data = {
+
+    api_key: '11361ae336b51f58679851b46306e28c',
+    language: 'it-IT',
+    query: title
+  };
+
+  $.ajax({
+
+    url: 'https://api.themoviedb.org/3/search/tv',
+    method: 'GET',
+    data: data,
+    success: function(data, state){
+      console.log(data);
+      ajaxSuccessSeries(data);
+
+    },
+    error: function(request, state, error){
+      console.log('error');
+    }
+
+  })
+}
+function ajaxSuccessSeries(data){
+  var res = data.results;
+
+  for (var i = 0; i < res.length; i++) {
+  var result = res[i];
+  var title = result.name;
+  var original = result.original_name;
+  var flag = getFlag(result.original_language);
+  var voto = Math.round(result.vote_average / 2);
+
+  console.log(voto);
+
+  addTitleSeries(title, original, flag, voto);
+  }
+}
+
+function addTitleSeries(title, original, flag, voto){
+
+
+  var tempData = {
+    title: title,
+    original: original,
+    flag: flag
+
+  }
+
+
+  var template = $('#box-template').html();
+  var compiled = Handlebars.compile(template);
+  var finalHTML = compiled(tempData);
+
+  var res = $('.series');
+  res.append(finalHTML);
+  addStar(voto);
+}
 
 function ajaxSuccess(data){
   var res = data.results;
@@ -51,17 +110,60 @@ function ajaxSuccess(data){
   var result = res[i];
   var title = result.title;
   var original = result.original_title;
-  var lang = result.original_language;
-  var voto = result.vote_average;
-  addTitle(title, original, lang, voto);
+  var flag = getFlag(result.original_language);
+  var voto = Math.round(result.vote_average / 2);
+
+  console.log(voto);
+
+  addTitle(title, original, flag, voto);
   }
 }
 
+function addStar(voto){
+ var filledStar = "<i class='fas fa-star'></i>";
+ var emptyStar = "<i class='far fa-star'></i>"
+
+  for (var i = 1; i <= 5; i++) {
+   if ( voto >= i) {
+
+     $('.star').last().append(filledStar);
+   }
+   else {
+     $('.star').last().append(emptyStar);
+   }
+
+  }
+}
+
+
+function getFlag(language){
+ var url;
+
+ switch(language){
+   case "it":
+    url = "http://www.adesivi4x4.it/1395-thickbox_default/bandiera-italiana.jpg";
+     break;
+   case "en":
+    url = "http://www.adesivi4x4.it/425-large_default/bandiera-inglese.jpg"
+
+     break;
+   case "es":
+    url = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Flag_of_Spain.svg/2000px-Flag_of_Spain.svg.png";
+
+     break;
+   default:
+    url = "https://images.emojiterra.com/twitter/v11/512px/2753.png"
+ }
+ return url;
+}
+
+
 function init(){
 $('#button').click(function(){
-  $('li').remove();
+  $('.films').remove();
   var input = $('#query').val();
   ajaxCall(input);
+  ajaxSeries(input);
   $('#query').val('');
 
 })
